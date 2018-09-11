66120edbab37e5d791dfa4482dae983bdd59754a
İsmail Reşat Akcan <iresatakcan.bm@gmail.com>
2018-09-11 22:31:10 +0300

    initial commit
    Projects added us repo.
    
    1-TicketSystem.API
    2-TicketSystem.Core
    3-TicketSystem.Data

---
+++ TicketSystem/TicketSystem.Data/Models/Entities/Tickets.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketSystem.Data.Models.Entities
+{
+    public class Tickets
+    {
+        public int ID { get; set; }
+
+        public string TicketTitle { get; set; }
+
+        public string TicketDescription { get; set; }
+    }
+}
