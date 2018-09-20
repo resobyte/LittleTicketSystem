32c0e2f892fdc848cbdd6e1fc25825bd504bb908
İsmail Reşat Akcan <iresatakcan.bm@gmail.com>
2018-09-20 19:15:37 +0300

    Notification model added

---
+++ TicketSystem/TicketSystem.Data/Models/Entities/Notifications.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketSystem.Data.Models.Entities
+{
+    public class Notifications
+    {
+        public int ID { get; set; }
+
+        public int NotificationID { get; set; }
+
+        public int NotificationTitle { get; set; }
+
+        public int NotificationDescription { get; set; }
+
+
+    }
+}
