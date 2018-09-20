e61b962615545b2692de2c13b5e7f34ef2fcee2a
İsmail Reşat Akcan <iresatakcan.bm@gmail.com>
2018-09-20 19:13:32 +0300

    Mail model added

---
+++ TicketSystem/TicketSystem.Data/Models/Entities/Mail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketSystem.Data.Models.Entities
+{
+    public class Mail
+    {
+        public int ID{ get; set; }
+
+        public int MailTitle { get; set; }
+
+        public int MailDescription { get; set; }
+    }
+}
