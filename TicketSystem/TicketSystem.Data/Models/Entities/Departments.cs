07e1a75cd9b27458c3a687a59d56738581e2fe73
İsmail Reşat Akcan <iresatakcan.bm@gmail.com>
2018-09-20 19:09:27 +0300

    Departments model added

---
+++ TicketSystem/TicketSystem.Data/Models/Entities/Departments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketSystem.Data.Models.Entities
+{
+    public class Departments
+    {
+        public int ID { get; set; }
+
+        public string DepartmentName { get; set; }
+    }
+}
