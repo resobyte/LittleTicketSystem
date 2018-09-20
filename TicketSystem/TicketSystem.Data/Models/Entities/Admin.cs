4bdaec6408d7847b13375280afe01a5efdc65724
İsmail Reşat Akcan <iresatakcan.bm@gmail.com>
2018-09-20 19:10:39 +0300

    Admin model added

---
+++ TicketSystem/TicketSystem.Data/Models/Entities/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketSystem.Data.Models.Entities
+{
+    public class Admin
+    {
+        public int ID { get; set; }
+
+        public int AdminUserName { get; set; }
+
+        public int AdminPassword { get; set; }
+    }
+}
